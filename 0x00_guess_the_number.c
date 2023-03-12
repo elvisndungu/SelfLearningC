3ba0ad42b59c6f7b917b84bf3e3e746c8e35c838
elvisndungu <senkutoshi@gmail.com>
2023-03-12 07:13:42 +0200

    v1 done

---
+++ 0x00_guess_the_number.c
@@ -0,0 +1,52 @@
+// Guess the number v0x01 by Elvis
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+
+
+int main() {
+    char nickname[30];
+    int guess;
+    int userGuesses;
+
+    bool run = true;
+
+    srand(time(NULL));  // Seed the random number generator
+    int randNum = (rand()%50)+1;
+
+    printf("Welcome to my first C game!!\n");
+    printf("What should I call you?\n");
+
+    printf("=> You can call me: ");
+    scanf("%s", nickname);
+    
+    printf("Hello, %s!\n", nickname);
+    printf("Lets play a guessing name;\n");
+    printf("You'll guess a number from 1-50/n");
+    printf("And I'll tell you if its too hot or cold\n");
+    
+    while(run) {
+
+        printf("Guess: ");
+        scanf("%d", &guess);
+
+        if (guess > randNum) {
+            printf("Hot!!\n");
+
+        } else if (guess < randNum) {
+            printf("Cold!!\n");
+
+        } else {
+            printf("Yesss\n");
+            run=false;
+
+        }
+        
+    }
+
+
+    return 0;
+}
